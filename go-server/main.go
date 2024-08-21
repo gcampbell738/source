aed54bdbfb027e02134dbbe71d220652a8da95ad
gcampbell738 <gcampbell738@yahoo.com>
2024-08-22 00:49:06 +0100

    formed methods

--- go-server/main.go
+++ go-server/main.go
@@ -1,7 +1,27 @@
-package main	
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	
+    fileServer := http.FileServer(http.Dir("./static"))
+    http.Handle("/", fileServer)
+    http.HandleFunc("/formHandler", formHandler)
+    http.HandleFunc("/hello", helloHandler)
+
+    fmt.Printf("Server running on port 8080...\n")
+    err := http.ListenAndServe(":8080", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello, World!")
 }
