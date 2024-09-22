3793cedc1df1a510eaa81d4c0b2ba1f21059c26e
gcampbell738 <gcampbell738@yahoo.com>
2024-09-22 23:26:10 +0100

    Working

--- go-server/main.go
+++ go-server/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
     fileServer := http.FileServer(http.Dir("./static"))
     http.Handle("/", fileServer)
-    http.HandleFunc("/formHandler", formHandler)
+    http.HandleFunc("/form", formHandler)
     http.HandleFunc("/hello", helloHandler)
 
     fmt.Printf("Server running on port 8080...\n")
@@ -20,8 +20,29 @@ func main() {
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
+    if err := r.ParseForm(); err != nil{
+        fmt.Fprintf(w,"Parsing form error: %v", err)
+        return
+    }
+
+    fmt.Fprintf(w,"Form Posted")
+    name := r.FormValue("Name")
+    email := r.FormValue("Email")
+
+    fmt.Fprintf(w,"Name: %v\nEmail:%v",name,email)
 }
 
+
 func helloHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/hello" {
+        http.Error(w, "Invalid Path",http.StatusNotFound)
+        return
+    }
+
+    if r.Method != http.MethodGet {
+        http.Error(w,"Not a get method",http.StatusNotFound)
+    }
+    
     fmt.Fprintf(w, "Hello, World!")
+
 }
